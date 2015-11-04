ae3c7fa5ea6c6bebb61c7f821da95ac57d6da92e
Jimmy Sáenz <jimmysaenz5@gmail.com>
2015-11-04 08:51:54 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var webpack = require('webpack');
+ 
+module.exports = {
+  entry: './main.js',
+  output: { path: __dirname, filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
